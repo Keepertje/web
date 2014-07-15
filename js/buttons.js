d455d1a6c2de13d2af15b22ff06c2ae1f4273c98
Cindy Berghuizen <c.berghuizen@gmail.com>
2014-07-15 12:50:54 +0200

    start

---
+++ js/buttons.js
@@ -0,0 +1,21 @@
+function onColorwipeClick() {
+	jQuery.get("/colorwipe");
+}(jQuery);
+
+function onPulseClick() {
+	jQuery.get("/pulse");
+}(jQuery);
+
+function Pixelwipe() {
+	jQuery.get("/pixelwipe");
+}(jQuery);
+
+function Rainbowwipe() {
+	jQuery.get("/rainbow");
+}(jQuery);
+
+function onNothingClick() {
+	jQuery.get("/nothing");
+}(jQuery);
+  
+  
